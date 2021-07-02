acf06dc16878532fd4847d3e4c482398f10f79b4
Eduardo Moura <50970652+mouraeduardo@users.noreply.github.com>
2021-07-02 19:02:34 -0300

    atualização em "Program"

--- api_rest/Program.cs
+++ api_rest/Program.cs
@@ -1,4 +1,4 @@
-/*using api_rest.Persistence.Context;
+using api_rest.Persistence.Context;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,41 +27,5 @@ namespace api_rest
             .UseStartup<Startup>()
             .Build();
     }
-}*/
-
-using api_rest;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using api_rest.Persistence.Context;
-
-namespace WebApplication
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = BuildWebHost(args);
-
-            using (var scope = host.Services.CreateScope())
-            using (var context = scope.ServiceProvider.GetService<AppDbContext>())
-            {
-                context.Database.EnsureCreated();
-            }
-
-            host.Run();
-        }
-
-        private static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
-    }
 }
+
